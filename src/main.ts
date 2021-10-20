d440cc03a17b952f61318549dc9abe2770966bb4
ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-10-21 00:40:23 +0100

    trying to make cors work in prod

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
-  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -26,6 +25,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.enableCors({});
 
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT);
