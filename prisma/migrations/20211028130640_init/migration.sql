a32a5d5fe588c8580614fd8ffa8fe720746ed57a
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-10-28 14:09:03 +0100

    changed default images url

---
+++ prisma/migrations/20211028130640_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "cover_img" SET DEFAULT E'https://res.cloudinary.com/dizyob2oz/image/upload/q_10/v1635426295/cover_image.jpg',
+ALTER COLUMN "profile_img" SET DEFAULT E'/https://res.cloudinary.com/dizyob2oz/image/upload/q_10/v1635425995/profile_img.jpg';
